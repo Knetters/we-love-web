fd21410ac14848f2271b34ff832d1c2da8a22aa6
ThomasScheepers <thomas@scheepers.com>
2022-12-14 13:02:23 +0100

    Spaties en json

--- scripts/script.js
+++ scripts/script.js
@@ -5,17 +5,18 @@ $(".color-content").mouseenter(function() {
 
 //Json color-content inladen
 const post_El = document.querySelector(".json-post-container");
+const post_Cont = document.querySelector(".post-container");
 
 fetch("https://knetters.github.io/we-love-web/scripts/posts.json")
   .then(res => res.json())
   .then(data => {
     data.forEach(post => {
       post_El.insertAdjacentHTML("beforeend", `
-        <div onclick="showPost(${post.Id})" class="flex-items color-content ${post.color}">
+        <div onclick="showPost(${post.id})" class="flex-items color-content ${post.color}">
           <div class="color-text-content">
             <p><i class="fa fa-adjust color-icon"></i></p>
             <div class="bottom-content">
-              <p class="post-number">0${post.Id}</p>
+              <p class="post-number">0${post.id}</p>
               <h2>${post.title}</h2>
               <p id="effect" class="top-border hidden">${post.intro}</p>
             </div>
@@ -26,5 +27,25 @@ fetch("https://knetters.github.io/we-love-web/scripts/posts.json")
   });
 
 function showPost(Id) {
-  alert("Post: " + Id)
+  let insertedContent = document.querySelector(".insertedContent");
+    if(insertedContent) {
+      insertedContent.parentNode.removeChild(insertedContent);
+    }
+
+  fetch("https://knetters.github.io/we-love-web/scripts/posts.json")
+  .then(res => res.json())
+  .then(data => {
+    data.forEach(post => {
+      if (post.id == Id) {
+      post_Cont.insertAdjacentHTML("beforeend", `
+        <div class="insertedContent">
+          <h2>${post.title}</h2>
+          <p>Door ${post.speaker}</p>
+          <p>Door ${post.content}</p>
+        </div>
+      `);
+      }
+    });
+  });
+  location.href = '/#posts';
 }
