daf9d7bb29207a0885b15435ebd848e1befd5a3a
ThomasScheepers <thomas@scheepers.com>
2022-12-14 12:38:51 +0100

    Progressie

--- scripts/script.js
+++ scripts/script.js
@@ -11,11 +11,11 @@ fetch("https://knetters.github.io/we-love-web/scripts/posts.json")
   .then(data => {
     data.forEach(post => {
       post_El.insertAdjacentHTML("beforeend", `
-        <div class="flex-items color-content ${post.color}">
+        <div onclick="showPost(${post.Id})" class="flex-items color-content ${post.color}">
           <div class="color-text-content">
             <p><i class="fa fa-adjust color-icon"></i></p>
             <div class="bottom-content">
-              <p class="post-number">${post.Id}</p>
+              <p class="post-number">0${post.Id}</p>
               <h2>${post.title}</h2>
               <p id="effect" class="top-border hidden">${post.intro}</p>
             </div>
@@ -24,3 +24,7 @@ fetch("https://knetters.github.io/we-love-web/scripts/posts.json")
       `);
     });
   });
+
+function showPost(Id) {
+  alert("Post: " + Id)
+}
